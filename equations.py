09e5dfc62dc6d651816eafe38cf668598a2d2316
TairBusi <tairbosa@gmail.com>
2021-03-22 20:52:56 +0200

    finalmatala1

--- equations.py
+++ equations.py
@@ -3,7 +3,7 @@ def exponent(x):
     numerator=1
     assembly = 1
     answer = 1
-    while i < 100:
+    while (i < 100):
         numerator= numerator*x
         assembly= assembly*i
         answer= answer*(numerator/assembly)
@@ -13,7 +13,7 @@ def exponent(x):
 def Ln(x):
     i=1
     y=1
-    while i < 100:
+    while (i < 100):
         y=y+(2*((x-exponent(y))/(x+exponent(y))))
         i=i+1
     return y
@@ -23,16 +23,20 @@ def XtimesY(x,y):
         sum_= exponent(y*Ln(x))
         return sum_
     else:
-        x=(-1)*x
-        sum_= exponent(y*Ln(x))
-    return (-1)*sum_
+        x=((-1)*x)
+        if (x%2 == 0):
+            sum_= exponent(y*Ln(x))
+            return sum_
+        else: 
+            sum_= exponent(y*Ln(x))
+            return ((-1)*sum_)
 
 def sqrt(x,y):
     sqr_= XtimesY(x,(1/y))
-    return sqr_()
+    return sqr_
 
 def calculate(x):
-    sum_num = (XtimesY(2.71828182846, x)*XtimesY(2, x)*XtimesY(x, 1))
+    sum_num = (XtimesY(2.71828182846, x)*XtimesY(7, x)*XtimesY(x,-1))
     return float('%0.6f' % sum_num)
 
 try:
